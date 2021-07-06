d2658646f0a6a3d5c2eb02817660c8908756f355
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-07-06 23:03:43 +0800

    Remove extra code

--- src/plugins/core/help/_stackoverflow.js
+++ src/plugins/core/help/_stackoverflow.js
@@ -2,39 +2,6 @@ function helpMessage() {
   return {
     embed: {
       author: {
-        name: `Tatsu's Help Menu`,
-        url: "https://tatsubot.com",
-        icon_url:
-          "https://cdn.discordapp.com/avatars/172002275412279296/f5f65755f67ae1dc88d9bb271d0f5bef.jpg"
-      },
-      title: `**t!youtube**`,
-      description: `\`\`\`Searches for Youtube videos and displays them onto Discord.\`\`\`\n`,
-      fields: [
-        {
-          name: `***Examples***`,
-          value: `\`t!youtube\`\n Searches Youtube for a random video of any kind.
-                    \n \`t!youtube Tatsu\`\n Searches Youtube for a video with Tatsu in it.`,
-          inline: true
-        },
-        {
-          name: `***Usages***`,
-          value: `\`t!youtube\` \`t!youtube <search terms>\``,
-          inline: true
-        },
-        {
-          name: `***Aliases***`,
-          value: `___youtube___, ___yt___`
-        }
-      ],
-      color: 1548647
-    }
-  };
-}
-
-function helpMessage() {
-  return {
-    embed: {
-      author: {
         name: `Fliggy's Support Centre`,
         url: "https://google.com",
         icon_url:
