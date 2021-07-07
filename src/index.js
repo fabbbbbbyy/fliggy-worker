5a84c1c6a9a6b129a8928e657af3312020f1f5b1
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-07-07 23:57:10 +0800

    Add guild welcome message

--- src/index.js
+++ src/index.js
@@ -71,6 +71,19 @@ client.on("message", async message => {
     client.commands.get(command).handle(message, args, client);
 })
 
+client.on('guildCreate', guild => {
+    guild.systemChannel.send({
+        embed: {
+            "title": `${guild.name} -- Fliggy Established`,
+            "description": "Welcome **Fliggy**, we're glad to have you. :european_castle:\n\nTo get started, send `f!help`. Each command works this way, for example, `f!invite`.\n**Fliggy** helps you and your discord community, featuring:\n:people_wrestling:  **Server Management**\n:computer: **Coding Assistance**\n:man_factory_worker: **Gimmicks & Fun**\n\nDo check out the __[Support Server](https://discord.gg/2EbjnuqG)__ :helicopter: as well!",
+            "color": 9160786,
+            "thumbnail": {
+                "url": "https://cdn.discordapp.com/attachments/861239068401860660/862254178637709342/2465301.png"
+            }
+        }
+    })
+})
+
 client.login(process.env.BOT_TOKEN);
 
 module.exports = {
