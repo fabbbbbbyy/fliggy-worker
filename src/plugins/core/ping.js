a82e4693e45493f6c0283fbc1c410e6a01a11295
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-07-22 14:10:30 +0800

    Update ping.js

--- src/plugins/core/ping.js
+++ src/plugins/core/ping.js
@@ -1,22 +1,22 @@
 const { infoEmoji } = require("../../helpers/emojis");
 
 function handle(message, args) {
-    message
-    .reply({emoji: infoEmoji.main, content: "Pong!"})
-    .then(pongMessage => {
-        const pongTime = pongMessage.createdTimestamp - message.createdTimestamp;
-        return pongMessage.edit({
-            emoji: infoEmoji.main,
-            content: `${pongMessage.content} - Time taken: **${pongTime}ms**`
-        });
-    })
+  message
+    .reply({ emoji: infoEmoji.main, content: "Pong!" })
+    .then((pongMessage) => {
+      const pongTime = pongMessage.createdTimestamp - message.createdTimestamp;
+      return pongMessage.edit({
+        emoji: infoEmoji.main,
+        content: `${pongMessage.content} - Time taken: **${pongTime}ms**`,
+      });
+    });
 }
 
 const options = {
-    name: "ping"
+  name: "ping",
 };
 
 module.exports = {
-    handle, 
-    options  
+  handle,
+  options,
 };
