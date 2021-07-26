1876fea7e6540dd900044360eead039de258c0ab
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-07-26 15:09:19 +0800

    Update anime command with custom menu embed

--- src/plugins/fun/anime.js
+++ src/plugins/fun/anime.js
@@ -1,9 +1,6 @@
 const anilist = require("anilist-node");
-const {
-  MessageActionRow,
-  MessageMenu,
-  MessageMenuOption,
-} = require("discord-buttons");
+const discord = require("discord.js");
+const createMenu = require("../../helpers/pagination");
 const usageError = require("../../errors/_correctusage");
 
 const Anilist = new anilist();
@@ -28,106 +25,153 @@ async function handle(message, args, client) {
     );
   }
 
-  const messageMenuOptions = [];
+  let counter = 1;
+  const numEmbeds = Math.ceil(animu.length / 5);
+  const pages = [];
 
-  console.log(animu)
-
-  for (const anime of animu) {
-    const option = new MessageMenuOption()
-    .setLabel(anime.title.userPreferred.slice(0, 24))
-    .setDescription(anime.title.native)
-    .setValue(anime.id);
-
-    messageMenuOptions.push(option);
+  for (let i = 0; i < numEmbeds; i++) {
+    const fields = [];
+    const startIdx = counter - 1;
+    let endIdx = startIdx + 5;
+    if (i == numEmbeds - 1) {
+      endIdx = animu.length;
+    }
+    for (let j = startIdx; j < endIdx; j++) {
+      fields.push({
+        name: `**${counter}.** ${animu[j].title.userPreferred}`,
+        value: animu[j].title.native,
+      });
+      counter++;
+    }
+    const embed = new discord.MessageEmbed()
+      .setColor(174334)
+      .setAuthor("AniList Search Result", "https://avatars2.githubusercontent.com/u/18018524?s=280&v=4", "https://anilist.co/")
+      .setDescription(
+        `Here are your search results for: **${args}**\n Please enter a **number** corresponding to the result you want to view.`
+      )
+      .setThumbnail(
+        "https://cdn.discordapp.com/attachments/861239068401860660/862254178637709342/2465301.png"
+      )
+      .addFields(fields);
+    pages.push(embed);
   }
 
-  const Menu = new MessageMenu()
-    .setID("Anime Menu")
-    .setPlaceholder("Bring me up, drop me down!")
-    .setMaxValues(1)
-    .setMinValues(1);
+  const filter = (m) => m.author.id === message.author.id;
 
-  for (const option of messageMenuOptions) {
-      Menu.addOption(option);
-  }
-
-  const Row1 = new MessageActionRow().addComponent(Menu);
-
-  await message.reply("\`Please select the title that is relevant to your search.\`", { components: [Row1] });
-
-  client.on("clickMenu", async (menu) => {
-    menu.reply.defer();
-    const menuValue = parseInt(menu.values[0]);
-    const result = await Anilist.media.anime(menuValue);
-
-    message
-        .reply({
-          embed: {
-            title: `${result.title.userPreferred} | ${result.title.native}`,
-            description: `${result.description
-              .replace(/<br>/gi, "")
-              .replace(`[Written by MAL Rewrite]`, "")}`,
-            url: `https://anilist.co/anime/${result.id}`,
-            color: 174334,
-            thumbnail: {
-              url: `${result.coverImage.medium}`
-            },
-            author: {
-              name: "AniList Search Result",
-              url: "https://anilist.co/",
-              icon_url:
-                "https://avatars2.githubusercontent.com/u/18018524?s=280&v=4"
-            },
-            fields: [
-              {
-                name: "Episodes",
-                value: `${result.episodes || "?"}`,
-                inline: true
+  createMenu(message, pages).then((msg) => {
+    message.channel
+      .awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] })
+      .then(async (collected) => {
+        if (isNaN(collected.first().content)) {
+          return message
+            .reply({
+              content:
+                "Please provide a valid number for the corresponding option. The menu will now close.",
+            })
+            .then(() => {
+              msg.delete({
+                timeout: 0,
+              });
+            });
+        }
+        const number = parseInt(collected.first().content);
+        if (number >= 1) {
+          const selected = animu[number - 1].id;
+          const result = await Anilist.media.anime(selected);
+          return message
+            .reply({
+              embed: {
+                title: `${result.title.userPreferred} | ${result.title.native}`,
+                description: `${result.description
+                  .replace(/<br>/gi, "")
+                  .replace(`[Written by MAL Rewrite]`, "")
+                  .replace("`", "")}`,
+                url: `https://anilist.co/anime/${result.id}`,
+                color: 174334,
+                thumbnail: {
+                  url: `${result.coverImage.medium}`,
+                },
+                author: {
+                  name: "AniList Search Result",
+                  url: "https://anilist.co/",
+                  icon_url:
+                    "https://avatars2.githubusercontent.com/u/18018524?s=280&v=4",
+                },
+                fields: [
+                  {
+                    name: "Episodes",
+                    value: `${result.episodes || "?"}`,
+                    inline: true,
+                  },
+                  {
+                    name: "Type",
+                    value: `${result.format || "?"}`,
+                    inline: true,
+                  },
+                  {
+                    name: "Status",
+                    value: `${result.status.toLowerCase() || "?"}`,
+                    inline: true,
+                  },
+                  {
+                    name: "Duration",
+                    value: `${result.duration || "?"} minutes per episode`,
+                    inline: true,
+                  },
+                  {
+                    name: "Aired",
+                    value: `from **${result.startDate.year || "?"}-${
+                      result.startDate.month || "?"
+                    }-${result.startDate.day || "?"}** to **${
+                      result.endDate.year || "?"
+                    }-${result.endDate.month || "?"}-${
+                      result.endDate.day || "?"
+                    }**`,
+                  },
+                  {
+                    name: "Average Rating",
+                    value: `Rated **${
+                      result.meanScore || "?"
+                    }%** by all users!`,
+                    inline: true,
+                  },
+                  {
+                    name: "Popularity",
+                    value: `**${result.popularity || "?"}** users follow this!`,
+                    inline: true,
+                  },
+                  {
+                    name: "Genres",
+                    value: `${result.genres.join(", ") || "?"}\n`,
+                  },
+                ],
+                footer: {
+                  icon_url: `${process.env.SUPPORT_ICON_URL}`,
+                  text: `Type f!manga or f!manhwa to search for manga/manhwa instead!`,
+                },
               },
-              {
-                name: "Type",
-                value: `${result.format || "?"}`,
-                inline: true
-              },
-              {
-                name: "Status",
-                value: `${result.status.toLowerCase() || "?"}`,
-                inline: true
-              },
-              {
-                name: "Duration",
-                value: `${result.duration || "?"} minutes per episode`,
-                inline: true
-              },
-              {
-                name: "Aired",
-                value: `from **${result.startDate.year || "?"}-${result
-                  .startDate.month || "?"}-${result.startDate.day ||
-                  "?"}** to **${result.endDate.year || "?"}-${result.endDate
-                  .month || "?"}-${result.endDate.day || "?"}**`
-              },
-              {
-                name: "Average Rating",
-                value: `Rated **${result.meanScore || "?"}%** by all users!`,
-                inline: true
-              },
-              {
-                name: "Popularity",
-                value: `**${result.popularity || "?"}** users follow this!`,
-                inline: true
-              },
-              {
-                name: "Genres",
-                value: `${result.genres.join(", ") ||
-                  "?"}\n`
-              }
-            ],
-            footer: {
-              icon_url: `${process.env.SUPPORT_ICON_URL}`,
-              text: `Type f!manga or f!manhwa to search for manga/manhwa instead!`
-            }
-          }
-        })
+            })
+            .then(() => {
+              msg.delete({
+                timeout: 0,
+              });
+            });
+        } else {
+          return message
+            .reply({
+              content:
+                "Please provide a valid number for the corresponding option. The menu will now close.",
+            })
+            .then(() => {
+              msg.delete({
+                timeout: 0,
+              });
+            });
+        }
+      })
+      .catch((collected) => {
+        message.channel.send("Looks like you are undecided for now.");
+      });
   });
 }
 
